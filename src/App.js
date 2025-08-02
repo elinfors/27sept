9c1797432206b60f622cd2a4a3b3124b1ea2cd53
Elin <forsbergselin@gmail.com>
2025-08-02 15:33:41 +0200

    comment

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ function App() {
           Elin & Simon
           </div>
           <div className="header-date-small">
-          Vi ska gifta oss och hoppas att du vill fira det ihop med oss!
+          Vi ska gifta oss och hoppas att du vill fira det med oss!
           </div>
           {/*
           <div className="header-button">
@@ -116,7 +116,7 @@ function App() {
         <p className='Info-title'>BARN</p>       
             <p className="Section-description-info"> Alla, barn som vuxna, är hjärtligt välkomna till vigseln. Efterföljande middag och fest är dock till för vuxna. Ammande barn och spädbarn är självklart välkomna.</p>
           <p className='Info-title'>KLÄDKOD</p>       
-            <p className="Section-description-info"> Mörk kostym </p>
+            <p className="Section-description-info"> Mörk kostym. Svart kostym och röd klänning går jättebra att ha! </p>
         </div>
         <div className="Info-image-container">
           <img className="Church-image" src={cheers}></img>
